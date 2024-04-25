1a3f581a62975e74f9918bb1f90ddbdc386df8f5
lontraeye <lucascardozo73@gmail.com>
2024-04-25 18:25:54 -0300

    Initial commit

---
+++ src/test/java/com/lontra/crediario/CrediarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lontra.crediario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrediarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
